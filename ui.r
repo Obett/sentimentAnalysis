c9368a754dbd9cf27c68b88d08a80f29fba884cc
Obett <48832691+Obett@users.noreply.github.com>
2022-04-22 15:10:32 +0100

    Update ui.r

--- ui.r
+++ ui.r
@@ -28,24 +28,24 @@ shinyUI(fluidPage(
     fileInput("file", "Upload text file"),
     uiOutput('id_var'),
     uiOutput("doc_var"),
-    textInput("stopw", ("Enter stop words separated by comma(,)"), value = "will,can"),
+    textInput("stopw", ("Enter your own stopwords and use comma(,) to separate them"), value = "is, above"),
     
     # selectInput("ws", "Weighing Scheme", 
     #             c("weightTf","weightTfIdf"), selected = "weightTf"), # weightTf, weightTfIdf, weightBin, and weightSMART.
     #
     htmlOutput("pre_proc1"),
     htmlOutput("pre_proc2"),
-    sliderInput("freq", "Minimum Frequency in Wordcloud:", min = 0,  max = 100, value = 2),
+    sliderInput("freq", "Wordcloud Minimum Frequency:", min = 0,  max = 100, value = 2),
     
     sliderInput("max",  "Maximum Number of Words in Wordcloud:", min = 1,  max = 300,  value = 50),  
     
-    numericInput("nodes", "Number of Central Nodes in co-occurrence graph", 4),
-    numericInput("connection", "Number of Max Connection with Central Node", 5),
+    numericInput("nodes", "Co-occurrence graph with number of central nodes", 4),
+    numericInput("connection", "The maximum connection number with the central node", 5),
     
     
-    textInput("concord.word",('Enter word for which you want to find concordance'),value = 'good'),
-    checkboxInput("regx","Check for regex match"),
-    sliderInput("window",'Concordance Window',min = 2,max = 100,5),
+    textInput("concord.word",('If you want concordance for a word, please type that word here'),value = 'great'),
+    checkboxInput("regx","Find regex matches"),
+    sliderInput("window",'Consistency Window',min = 2,max = 100,5),
     
     
     actionButton(inputId = "apply",label = "Apply Changes", icon("refresh"))
@@ -56,22 +56,22 @@ shinyUI(fluidPage(
   mainPanel( 
     tabsetPanel(type = "tabs",
                 #
-                tabPanel("Overview & 20 Hotel Reviews Dataset",h4(p("How to use this App")),
+                tabPanel("Overview of 20 Hotel Reviews Dataset",h4(p("See my Data preparation steps below")),
                          
-                         p("To use this app you need a document corpus in txt file format. Make sure each document is separated from another document with a new line character.
-                           To do Sentiment Analysis Of 20 Hotels in your text corpus, click on Browse in left-sidebar panel and upload the txt file. Once the file is uploaded it will do the computations in 
-                            back-end with default inputs and accordingly results will be displayed in various tabs.", align = "justify"),
-                         p("If you wish to change the input, modify the input in left side-bar panel and click on Apply changes. Accordingly results in other tab will be refreshed
+                         p("Manually, I filtered 20 hotels in the excel sheet and extracted data of only Review column into a textpad and saved it as 20_Hotel_Reviews because I built this app in a way that it accepts only a document in txt file format. again, I ensured that each row of the reviews was separated from one another with a new line of character. Also, I have already attached my prepared dataset to be used below. Kind download it under Download text file and then, 
+                           on Browse in left side bar panel and upload the downloaded txt file which has been prepared. immediately the file has been uploaded, it will carry out the data processing in 
+                            backend with default inputs and show the results in different tabs.", align = "justify"),
+                         p("If you wish to modify the input, edit the input in left side bar panel and click on Commit Changes. The outputs in other tabs will be reprocessed.
                            ", align = "Justify"),
                          h5("Note"),
-                         p("You might observe no change in the outputs after clicking 'Apply Changes'. Wait for few seconds. As soon as all the computations
-                           are over in back-end results will be refreshed",
+                         p("The changes in output takes effect few seconds after clicking on 'Commit Changes'. This is to allow all the data processing
+                          in backend to complete before displaying the result",
                            align = "justify"),
                          #, height = 280, width = 400
                          verbatimTextOutput("start"),
                          h4(p("Download text file")),
                          downloadButton('downloadData1', '20_Hotel_Reviews reviews txt file'),br(),br(),
-                         p("Please note that download will not work with RStudio interface.  So open this app in a web-browser and then download the 20 Hotel Reviews file. For opening this app in web-browser click on \"Open in Browser\" as shown at top left of the browser. Below is Image is Samuel Obetta Shiny R programmer "),
+                        p("Kindly note that download is not supported with RStudio interface.  Thus use a web-browser to open this App and then download the 20 Hotel Reviews file. Please click on Open in Browser as shown at top left of the browser to open this App.")
                          img(src = "UniLogo.PNG")
                 )
                 ,
